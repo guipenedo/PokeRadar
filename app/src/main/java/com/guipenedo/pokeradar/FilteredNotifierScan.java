024236ad30415f5d596ad484b8d295f1c2fe2a99
PhilipsNostrum <nostrumg@gmail.com>
2016-07-27 13:08:13 +0100

    Created notifications branch

--- app/src/main/java/com/guipenedo/pokeradar/FilteredNotifierScan.java
+++ app/src/main/java/com/guipenedo/pokeradar/FilteredNotifierScan.java
@@ -26,7 +26,6 @@ public class FilteredNotifierScan extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
     }
 }
